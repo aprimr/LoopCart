07bc6550d86067911fa943b3695afaea2be81a76
aprimr <aprimregmi24@gmail.com>
2025-05-02 21:20:58 +0545

    swiper fixes

---
+++ client/src/components/Carousel.jsx
@@ -0,0 +1,13 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+const Carousel = () => {
+  return (
+    <section className="px-4 ">
+      <div className="max-w-screen-xl mx-auto">
+        <h1 className="text-white">Swiper</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Carousel;
